5556df1662566125e1db397e8f1495eb7ddc1469
Srushti Chikte <65661863+srush511@users.noreply.github.com>
2026-06-01 18:04:40 +0530

    Find the Smallest Missing Positive Integer

--- HackerRank Codes.py
+++ HackerRank Codes.py
@@ -47,3 +47,61 @@ if __name__ == '__main__':
     print(result)
   go
 
+
+"""
+Find the Smallest Missing Positive Integer
+Given an unsorted array of integers, find the smallest positive integer not present in the array in O(n) time and O(1) extra space.
+Example
+Input:
+orderNumbers = [3, 4, -1, 1]
+Output: 2
+"""
+
+
+## Solution
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'findSmallestMissingPositive' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY orderNumbers as parameter.
+#
+
+def findSmallestMissingPositive(orderNumbers):
+    listlength =len(orderNumbers)
+    for i in range(listlength):
+        while 1 <= int(orderNumbers[i]) <= listlength and orderNumbers[int(orderNumbers[i]) - 1] != orderNumbers[i]:
+            correct_idx = int(orderNumbers[i]) - 1
+            orderNumbers[i], orderNumbers[correct_idx] = orderNumbers[correct_idx], orderNumbers[i]
+    
+            
+    # Write your code here
+    for i in range(len(orderNumbers)):
+        if int(orderNumbers[i]) != i + 1:
+            return i + 1
+    return len(orderNumbers)+1
+
+if __name__ == '__main__':
+    orderNumbers_count = int(input().strip())
+
+    orderNumbers = []
+
+    for _ in range(orderNumbers_count):
+        orderNumbers_item = int(input().strip())
+        orderNumbers.append(orderNumbers_item)
+
+    result = findSmallestMissingPositive(orderNumbers)
+
+    print(result)
+go
+
